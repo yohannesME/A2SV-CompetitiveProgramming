4d431d70010227c9680ae8d993dfc5af3d3378f2
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2022-07-07 19:54:08 +0300

    Create find target indices after sorting array.py

---
+++ find target indices after sorting array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def targetIndices(self, nums, target):
+        nums.sort()
+        result = []
+        for i in range(len(nums)):
+            if(nums[i]==target):
+                result.append(i)
+
+        return result        
