7eeb08ac78f25dd9b9d03020a483b164870e8ee5
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2022-07-08 00:42:31 +0300

    Create implementing queue using stack.py

---
+++ implementing queue using stack.py
@@ -0,0 +1,31 @@
+class MyQueue(object):
+    queue = []
+
+    def __init__(self):
+        self.queue = []
+        
+
+    def push(self, x):
+        self.queue.append(x)
+        
+
+    def pop(self):
+        return self.queue.pop(0)
+
+        
+
+    def peek(self):
+        return self.queue[0]
+        
+
+    def empty(self):
+        return len(self.queue)==0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
