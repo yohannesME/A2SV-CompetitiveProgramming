fbb4a8106423d8ff22ce64982525f8aefec02103
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2022-09-27 13:08:24 +0300

    Create Basic Calculator II.py

---
+++ Basic Calculator II.py
@@ -0,0 +1,44 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        result = 0
+        current = prev = 0
+        
+        operation = '+'
+        
+        i = 0
+        
+        while i < len(s):
+            
+            if s[i].isdigit():
+                while i < len(s) and s[i].isdigit():
+                    current = current*10 + int(s[i])
+                    
+                    i+=1
+                i -= 1
+                
+                if operation == "+":
+                    result += current
+                    
+                    prev = current
+                elif operation == "-":
+                    result -= current
+                    
+                    prev = -current
+                elif operation == "*":
+                    result -= prev
+                    result += prev*current
+                    
+                    prev = current * prev
+                else:
+                    result -= prev
+                    result += int(prev/current)
+                    
+                    prev = int(prev/current)
+                
+                current = 0
+            elif s[i] != " ":
+                operation = s[i]
+            
+            i += 1
+        return result
+                
