fa8b192a0cb8f5265aeaa01609a27870786fa6c9
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2022-12-23 19:28:51 +0300

    Update python-lists.py

--- python-lists.py
+++ python-lists.py
@@ -1,7 +1,17 @@
 if __name__ == '__main__':
     N = int(input())
     arr = []
-    my_commands = {'insert': lambda index,value: arr.insert(index,value), 'print':lambda : print(arr), 'remove':lambda value: arr.remove(value),'reverse':lambda : arr.reverse(),'pop': lambda : arr.pop(),'append':lambda value: arr.append(value), 'sort': lambda: arr.sort()}
+    my_commands = {
+        'insert': lambda index,value: arr.insert(index,value), 
+        'print':lambda : print(arr), 
+        'remove':lambda value: arr.remove(value),
+        'reverse':lambda : arr.reverse(),
+        'pop': lambda : arr.pop(),'
+        append':lambda value: arr.append(value), 
+        'sort': lambda: arr.sort()
+    }
+    
+    #accept the input and process them by the use of the my_command hash map
     for _ in range(N):
         command = input().split()
         if len(command) == 1:
@@ -10,5 +20,3 @@ if __name__ == '__main__':
             my_commands[command[0]](int(command[1]))
         elif len(command) == 3:
             my_commands[command[0]](int(command[1]), int(command[2]))
-            
-        
