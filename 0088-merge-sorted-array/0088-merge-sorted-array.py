d4281f31972af87646e624deb2a0a007ab0e7b00
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2022-12-24 10:02:49 +0300

    Time: 34 ms (96.74%), Space: 13.8 MB (86.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,7 +1,30 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        nums1[m:] = nums2
-        nums1.sort()
+        #num1 bound tracker
+        num1RightBound = n+m -1
+        num1LeftBound = m-1
+        
+        #num2 bound trackers
+        num2RightBound = n-1
+        
+        
+        while num2RightBound > -1:
+            #if num1 is done parsing just replace what is left with num2
+            if num1LeftBound == -1:
+                nums1[:num2RightBound+1] = nums2[:num2RightBound+1]
+                break
+                
+            #if num2 is done parsing then just break the loop we are done
+            if num2RightBound == -1:
+                break
+            
+            if nums1[num1LeftBound] > nums2[num2RightBound]:
+                nums1[num1RightBound] = nums1[num1LeftBound]
+                num1LeftBound -= 1
+            else:
+                nums1[num1RightBound] = nums2[num2RightBound]
+                num2RightBound -= 1
+            
+            num1RightBound -= 1
+
+            
