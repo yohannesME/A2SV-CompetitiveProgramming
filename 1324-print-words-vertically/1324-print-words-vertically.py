c5a00917aa3c1c3a108acbad3cd9f619d590ef71
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-01-04 19:53:20 +0300

    Time: 42 ms (68.95%), Space: 13.8 MB (64.33%) - LeetHub

--- 1324-print-words-vertically/1324-print-words-vertically.py
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -7,20 +7,18 @@ class Solution:
         #split the word by space and zip the words veritcally 
         #zip_longest to constitute words that have larger length
         for vertical in zip_longest(*s.split(), fillvalue=' '):
-            #----------------------------------------------
-            #solution for this problem with out fill value
+
             #replace None with ' ' for words that are places when the word is long (None, None, None, 'T', None, None)
-            # vertical = list(map(lambda word: word if word != None else ' ', vertical))
-            #-------------------------------------------------
+            vertical = list(map(lambda word: word if word != None else ' ', vertical))
 
             #change the tuple returned into a string
-            vertical = ''.join(vertical)
+            word = ''.join(vertical)
             
             #remove space from the end of the string if None was replaced by it
-            vertical = vertical.rstrip()
+            word = word.rstrip()
             
             #append to input
-            output.append(vertical)
+            output.append(word)
             
         return output
         
