f6ac4362bc4479726ccff89875b1f9b12d4ada08
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-01-16 18:53:33 +0300

    Time: 42 ms (61.53%), Space: 13.8 MB (98.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,13 +1,10 @@
 class Solution:
-    
-    
     def rotate(self, matrix: List[List[int]]) -> None:
-        
         #row column size
         rowSize = len(matrix)
         columnSize = len(matrix[0])
         
-
+        #REVERSE THE COLUMN
         column = 0
         while column < columnSize:
             #last and first element in the row
@@ -21,14 +18,17 @@ class Solution:
             
             column += 1
         
+        #used to move to the next diagonal
         diagonal = 0
+        
         #first reverse the column
         for row in range(rowSize):
             for column in range(diagonal,columnSize):
                 #swap row and column
                 matrix[row][column], matrix[column][row] = matrix[column][row], matrix[row][column]
+                
+            #move to the next diagonal
             diagonal += 1
-            row += diagonal
             
             
             
