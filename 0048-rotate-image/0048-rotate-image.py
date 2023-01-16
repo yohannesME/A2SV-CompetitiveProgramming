eb0d3f38b6ddc2e1d4e647e06bd457cb583df153
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-01-16 18:29:39 +0300

    Time: 36 ms (83.63%), Space: 13.9 MB (70.45%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,59 @@
+class Solution:
+    
+    
+    def rotate(self, matrix: List[List[int]]) -> None:
+        
+        #row column size
+        rowSize = len(matrix)
+        columnSize = len(matrix[0])
+        
+
+        column = 0
+        while column < columnSize:
+            #last and first element in the row
+            rowleft = 0
+            rowright = rowSize - 1
+            #traverse and swap
+            while rowleft < rowright:
+                matrix[rowleft][column], matrix[rowright][column] = matrix[rowright][column], matrix[rowleft][column]
+                rowleft += 1
+                rowright -= 1
+            
+            column += 1
+        
+        diagonal = 0
+        #first reverse the column
+        for row in range(rowSize):
+            for column in range(diagonal,columnSize):
+                #swap row and column
+                matrix[row][column], matrix[column][row] = matrix[column][row], matrix[row][column]
+            diagonal += 1
+            row += diagonal
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+                
+                
+        
+        
