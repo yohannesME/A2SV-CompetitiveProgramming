9b16f228b4c6b69bf192d828a140b21213cbae58
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-01-17 18:37:18 +0300

    Time: 475 ms (12.68%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,13 +1,26 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
+        
         #sort the name as well when we are sorting the height
-        #SELECTION SORT
+        
+#         #BUBBLE SORT
+#         heightSize = len(heights)
+#         for i in range(heightSize):
+#             for j in range(0,heightSize-1-i):
+#                 if heights[j] < heights[j+1]:
+#                     heights[j], heights[j+1] = heights[j+1], heights[j]
+#                     names[j+1], names[j] = names[j], names[j+1]
+
+#         return names
+            
+        
+#         #SELECTION SORT
         heightSize = len(heights)
         for i in range(heightSize):
             #find the maximum number in the array and record its index
             maximum = heights[i]
-            index = -1
+            index = i
             
             #iterate over the array and record the largest element index
             for j in range(i+1,heightSize):
@@ -16,9 +29,14 @@ class Solution:
                     index = j
                     
             #check if more larger element was found     
-            if index != -1:
-                heights[i], heights[index] = heights[index], heights[i]
-                names[i], names[index] = names[index], names[i]
+            
+            heights[i], heights[index] = heights[index], heights[i]
+            names[i], names[index] = names[index], names[i]
 
         return names
+                    
+         
+        
+    
+    
             
