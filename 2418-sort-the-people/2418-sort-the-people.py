c11f443c2bfa772704c9b0fe584da4b3ce780ddb
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-01-17 19:02:35 +0300

    Time: 1481 ms (5.45%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -2,7 +2,21 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
         
-        #sort the name as well when we are sorting the height
+          #sort the name as well when we are sorting the height
+        #INSERTION SORT
+        heightSize = len(heights)
+        
+        for i in range(1,heightSize):
+            move = 0
+            for j in range(i-1,-1,-1):
+                if heights[i-move] > heights[j]:
+                    heights[i-move], heights[j] = heights[j], heights[i-move]
+                    names[i-move], names[j] = names[j], names[i-move]     
+                    move += 1
+                else:
+                    break
+        
+        return names
         
 #         #BUBBLE SORT
 #         heightSize = len(heights)
@@ -16,24 +30,23 @@ class Solution:
             
         
 #         #SELECTION SORT
-        heightSize = len(heights)
-        for i in range(heightSize):
-            #find the maximum number in the array and record its index
-            maximum = heights[i]
-            index = i
+#         heightSize = len(heights)
+#         for i in range(heightSize):
+#             #find the maximum number in the array and record its index
+#             maximum = heights[i]
+#             index = i
             
-            #iterate over the array and record the largest element index
-            for j in range(i+1,heightSize):
-                if heights[j] > maximum:
-                    maximum = heights[j]
-                    index = j
+#             #iterate over the array and record the largest element index
+#             for j in range(i+1,heightSize):
+#                 if heights[j] > maximum:
+#                     maximum = heights[j]
+#                     index = j
                     
-            #check if more larger element was found     
-            
-            heights[i], heights[index] = heights[index], heights[i]
-            names[i], names[index] = names[index], names[i]
+#             #check if more larger element was found     
+#             heights[i], heights[index] = heights[index], heights[i]
+#             names[i], names[index] = names[index], names[i]
 
-        return names
+#         return names
                     
          
         
