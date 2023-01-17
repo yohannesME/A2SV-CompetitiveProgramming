9cf6700aef1fe3a853867835dd8b7bc7f22500e7
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-01-17 18:29:37 +0300

    Time: 502 ms (12.09%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -2,12 +2,23 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
         #sort the name as well when we are sorting the height
+        #SELECTION SORT
         heightSize = len(heights)
         for i in range(heightSize):
-            for j in range(0,heightSize-1-i):
-                if heights[j] < heights[j+1]:
-                    heights[j], heights[j+1] = heights[j+1], heights[j]
-                    names[j+1], names[j] = names[j], names[j+1]
+            #find the maximum number in the array and record its index
+            maximum = heights[i]
+            index = -1
+            
+            #iterate over the array and record the largest element index
+            for j in range(i+1,heightSize):
+                if heights[j] > maximum:
+                    maximum = heights[j]
+                    index = j
+                    
+            #check if more larger element was found     
+            if index != -1:
+                heights[i], heights[index] = heights[index], heights[i]
+                names[i], names[index] = names[index], names[i]
 
         return names
             
