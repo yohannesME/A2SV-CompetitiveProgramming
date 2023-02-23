f1ddb8a34d08b8741acd2a4d87578346e48a5767
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-02-22 23:02:29 -0800

    Time: 1347 ms (35.46%), Space: 26.2 MB (14.19%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        #calculative Variable
+        N = len(nums)
+        #the average and the sum of the k elements
+        maxAvg = float('-inf')
+        subArrSum = 0
+        #boundary to our sliding window
+        leftBound = 0
+        rightBound = k-1
+        
+        #create the slide window
+        for i in range(k):
+            subArrSum += nums[i]
+    
+        
+        #move the window and calculate the average
+        while leftBound < N and rightBound < N:
+            maxAvg = max(maxAvg, subArrSum/k)
+            subArrSum -= nums[leftBound]
+            leftBound += 1
+            rightBound += 1
+            if rightBound >= N:
+                break
+            subArrSum += nums[rightBound]
+            
+            
+        
+        return maxAvg
+        
