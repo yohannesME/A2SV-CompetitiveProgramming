4ac10deb2b778aa08a41b9061eac8342957e9f62
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-02-28 04:49:46 -0800

    Time: 35 ms (49.56%), Space: 13.9 MB (10.62%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -4,6 +4,7 @@ class Solution:
             return [1]*(rowIndex+1)
         else:
             row = [1]
+            #get the last rowIndex recursively
             triangle = self.getRow(rowIndex-1)
             for i in range(len(triangle)-1):
                 row.append(triangle[i]+triangle[i+1])
