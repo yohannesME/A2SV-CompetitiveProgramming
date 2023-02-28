98548a3c57d5845673211b0a0e3a55416972045b
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-02-28 04:44:15 -0800

    Time: 32 ms (68.20%), Space: 13.9 MB (10.62%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex < 2:
+            return [1]*(rowIndex+1)
+        else:
+            row = [1]
+            triangle = self.getRow(rowIndex-1)
+            for i in range(len(triangle)-1):
+                row.append(triangle[i]+triangle[i+1])
+            row.append(1)
+            return row
