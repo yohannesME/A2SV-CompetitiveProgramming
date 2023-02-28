2d5e68c8713b1ca62cc2955c2419a083aa0ffe47
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-02-28 03:50:22 -0800

    Time: 204 ms (62.09%), Space: 43.4 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,13 +1,11 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        #two variable at the begining and end of the array and move inward to the array
-        leftBound = 0
-        rightBound = len(s)-1
+        def reverse(left,right):
+            if left >= right:
+                return
+            s[left], s[right] = s[right],s[left]
+            reverse(left+1, right-1)
         
-        while leftBound < rightBound:
-            #swap the two opposite side of the array
-            s[leftBound], s[rightBound] = s[rightBound], s[leftBound]
-            
-            #increment and decrement the left and right Bound
-            rightBound -= 1
-            leftBound += 1
+        reverse(0, len(s)-1)
+        return s
+
