2ebda4610bb827a3e94a603480cc4a3328284c09
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-03-05 09:05:24 -0800

    added search-insert-position.python

--- search-insert-position.py
+++ search-insert-position.py
@@ -1,3 +1,17 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return bisect_left(nums,target)
+        # return bisect_left(nums,target)
+        N = len(nums)
+        left = 0
+        right = N-1
+
+        while left < right:
+            mid = (right+left)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left + 1 if nums[left] < target else left
