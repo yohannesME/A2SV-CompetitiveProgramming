cda00e7d3002d5cea354fee9290c546d704e892e
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-03-06 07:09:18 -0800

    added kth-missing-positive-number.python

---
+++ kth-missing-positive-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        N = len(arr)
+        left = 0
+        right = N-1
+
+        while left < right:
+            mid = (left+right)//2
+
+            if arr[mid] - mid - 1 < k:
+                left = mid + 1
+            else:
+                right = mid
+
+        if arr[right] - right - 1 < k:
+            return  k  + right + 1
+        else:
+            if right == 0:
+                return k
+            else:
+                return arr[right-1] + k - (arr[right-1] - right )
+
+
+        
+        return right
