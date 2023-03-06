11b13ed90b2d9fd6b03e04df3431e8db98872660
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-03-05 23:41:07 -0800

    added longest-repeating-character-replacement.python

---
+++ longest-repeating-character-replacement.py
@@ -0,0 +1,27 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        #inial values used sliding window, length, answer
+        longestRep = 0 
+        N = len(s)
+        rightBound = 0
+        leftBound = 0
+        
+        #count the occurance
+        letterCount = defaultdict(int)
+        
+        #until we reach the point we have no more k moves we check and update longest
+        while rightBound < N:
+            letterCount[s[rightBound]] += 1
+            
+            maxRep =max(list(letterCount.values()))
+            
+            if rightBound - leftBound + 1 - maxRep > k:
+                letterCount[s[leftBound]] -= 1
+                leftBound += 1
+            else:
+                longestRep = max(longestRep,rightBound - leftBound + 1 )
+            
+            rightBound += 1
+
+        return longestRep
