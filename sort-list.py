094073c046c6823fb05291c3016898ea9455cec9
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-03-12 12:25:40 -0700

    added sort-list.python

--- sort-list.py
+++ sort-list.py
@@ -4,52 +4,50 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    #finds the middle of the current linked list
+    def findMiddle(self,head):
+        #this variable is used to cut of the head pointer right before the middle one
+        prevTotortoise = head
+        #straight forward tortoise and rabbit algorithm to find the middle
+        tortoise = head
+        rabbit = head
     
+        while rabbit and rabbit.next:
+            prevTotortoise = tortoise
+            tortoise = tortoise.next
+            rabbit = rabbit.next.next
+        
+        prevTotortoise.next = None
+        return tortoise
+
+    #merge two sorted list
+    def sortedListMerge(self,list1, list2):
+        dummy = cur = ListNode(0)
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        cur.next = list1 or list2
+        return dummy.next
+
+    #cut the list into half each time until there is only one element and build it up again but this time sorted.
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        sortedList = None
-
-        def findMiddle(head):
-            tortoise = head
-            prevTotortoise = head
-            rabbit = head
+        if not head:
+            return
         
-            while rabbit and rabbit.next:
-                prevTotortoise = tortoise
-                tortoise = tortoise.next
-                rabbit = rabbit.next.next
-            
-            prevTotortoise.next = None
-            return tortoise
+        if not head.next:
+            return head
         
-        def sortedListMerge(list1, list2):
-            dummy = cur = ListNode(0)
-            while list1 and list2:
-                if list1.val < list2.val:
-                    cur.next = list1
-                    list1 = list1.next
-                else:
-                    cur.next = list2
-                    list2 = list2.next
-                cur = cur.next
-            cur.next = list1 or list2
-            return dummy.next
+        middle = self.findMiddle(head)
 
-        def sortlinkedlist(head):
-            if not head:
-                return
-            
-            if not head.next:
-                return head
-            
-            middle = findMiddle(head)
+        if not middle:
+            return
 
-            if not middle:
-                return
+        secondHalf = self.sortList(middle)
+        firstHalf = self.sortList(head)
 
-            secondHalf = sortlinkedlist(middle)
-            firstHalf = sortlinkedlist(head)
-
-            return sortedListMerge(firstHalf, secondHalf)
-
-
-        return sortlinkedlist(head)
+        return self.sortedListMerge(firstHalf, secondHalf)
