e73d5b27840ce3ff7bcbc7da6ecce3581250aef7
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-04-28 09:10:11 -0700

    added keys-and-rooms.python

---
+++ keys-and-rooms.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        queue = rooms[0][:]
+        visited = set([0])
+        visited.add(0)
+
+        while queue:
+            room = queue.pop()
+    
+            for key in rooms[room]:
+                if key not in visited:
+                    queue.append(key)
+            visited.add(room)
+                        
+        return len(rooms) == len(visited)
