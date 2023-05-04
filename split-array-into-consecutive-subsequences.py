96b0b088ad070afe9087961e7192712ad473db76
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-05-04 10:07:42 -0700

    added split-array-into-consecutive-subsequences.python

--- split-array-into-consecutive-subsequences.py
+++ split-array-into-consecutive-subsequences.py
@@ -2,21 +2,16 @@ class Solution:
     def isPossible(self, nums: List[int]) -> bool:
         heapArr = []
 
-        nums = [-num for num  in nums]
-
         for num in nums:
             foundPlace = False
 
             for i in range(len(heapArr)-1, -1,-1):
-                if num+1 == heapArr[i][0]:
-                    heappush(heapArr[i], num)
+                if num-1 == heapArr[i][-1]:
+                    heapArr[i].append(num)
                     foundPlace = True
                     break
             
             if not foundPlace:
                 heapArr.append([num])
 
-        for heap in heapArr:
-            if len(heap) < 3:
-                return False
-        return True
+        return all(list(map(lambda x: len(x)>2, heapArr)))
