384ca1d3c2dea5fa4b76c0c953061290493cb086
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-10-06 10:23:26 +0300

    added course-schedule-iv.python

--- course-schedule-iv.py
+++ course-schedule-iv.py
@@ -1,29 +1,20 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        adjList = defaultdict(list)
+        # get the array
+        dist = [[False] * numCourses for _ in range(numCourses)]
+
+        for i, j in prerequisites:
+            dist[i][j] = True
+
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+
+                    dist[i][j] = dist[i][j] or (dist[i][k] and dist[k][j])
+        
         ans = []
-        indegree = [0]*numCourses
 
-        for v1, v2 in prerequisites:
-            adjList[v1].append(v2)
-            indegree[v1] += 1
-        
-        def dfs(node, target, visited):
-            visited.add(node)
-
-            if node == target:
-                return True
-
-            for child in adjList[node]:
-                if child not in visited:
-                    if dfs(child, target, visited):
-                        return True
-            return False
-        
-        for v1, v2 in queries:
-            if indegree[v1] == 0:
-                ans.append(False)
-            else:
-                ans.append(dfs(v1,v2, set()))
+        for i,j in queries:
+            ans.append(dist[i][j])
         
         return ans
